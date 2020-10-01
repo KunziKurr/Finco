e4aac7696309460cd5e6095bcc9dc3b9ea7f55e5
Shields10 <benjaminshields10@gmail.com>
2020-10-01 19:49:40 +0300

    Added pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,19 @@
 // Custom JS TO Write own JS
+function fnRedirectToHomePage(){
+    window.location.href = 'index.html';
+}
+function fnRedirectToServicesPage(){
+    window.location.href = 'services.html';
+}
+function fnRedirectToOurTeamPage(){
+    window.location.href = 'ourteam.html';
+}
+function fnRedirectToPricingPage(){
+
+}
+function fnRedirectToStrategyPage(){
+    window.location.href = 'strategy.html';
+}
+function fnRedirectToContactUsPage(){
+    window.location.href = 'contactus.html';
+}
